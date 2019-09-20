38f9198d1998c07edad97c14a06afc6d6ff457a4
HoangTungSon <hoangtungson164@gmail.com>
2019-09-20 17:08:19 +0700

    first commit - user/admin

---
+++ security_spring/src/main/java/source/MvcWebApplicationInitialization.java
@@ -0,0 +1,20 @@
+package source;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitialization extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {WebSecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
