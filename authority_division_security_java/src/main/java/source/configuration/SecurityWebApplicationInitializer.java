6709c2f641ff4c753a318916f4c4963474117000
HoangTungSon <hoangtungson164@gmail.com>
2019-09-23 09:19:02 +0700

    second commit

---
+++ authority_division_security_java/src/main/java/source/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package source.configuration;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
